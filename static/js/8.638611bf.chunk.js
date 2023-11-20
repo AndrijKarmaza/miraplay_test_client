156eda4a8a7dff24a523ead9627299a1e85e293e
AndrijKarmaza <andrij.karmaza@gmail.com>
2023-11-20 16:23:31 +0200

    Updates

---
+++ static/js/8.638611bf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmiraplay_test_client=self.webpackChunkmiraplay_test_client||[]).push([[8],{8:(r,s,e)=>{e.r(s),e.d(s,{default:()=>o});var t=e(87);const a="Error_error__CkOcQ",c="Error_title__x1xEd",_="Error_descr__s1tPX",l="Error_error_btn__br10R";var i=e(184);const o=()=>(0,i.jsxs)("div",{className:a,children:[(0,i.jsx)("h1",{className:c,children:"error"}),(0,i.jsx)("p",{className:_,children:"Oops, this page doesn't exist!"}),(0,i.jsx)(t.rU,{to:"/",className:l,children:"Home"})]})}}]);
+//# sourceMappingURL=8.638611bf.chunk.js.map
